b2713ef0d73ff8832f5d9c19987716622e6f6149
Gerardo J Garcia <gerardojgarcia@pm.me>
2022-12-28 15:48:18 -0800

    figured out how to make a slider with current number indicator

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + '' + middleInitial + '' + lastName;
+    }
+    return Student;
+}());
 var containerEl = document.getElementById('container');
-containerEl.innerHTML = "<h1 class=\"title\">Hello Everybody this is typescript practice</h1>";
-var titleEl = document.querySelector('.title');
-console.log(titleEl);
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Gerardo Garcia";
-document.body.textContent = greeter(user);
+var btn = document.querySelector('.button');
+var number = 0;
+var displayNumber = document.querySelector('.display-number');
+var btnReset = document.querySelector('.button-reset');
+btnReset.addEventListener('click', function () {
+    number = 0;
+    var resetNumber = number.toString();
+    displayNumber.textContent = resetNumber;
+});
+btn.addEventListener("click", function () {
+    console.log('i was clicked', number);
+    if (number >= 10) {
+        number = 0;
+    }
+    else {
+        number++;
+    }
+    var newNumber = number.toString();
+    displayNumber.textContent = newNumber;
+});
+var sliderCount = document.querySelector('.slider-number');
+var slider = document.querySelector('#slider');
+slider.addEventListener('input', function () {
+    var sliderValue = this.value;
+    sliderCount.textContent = sliderValue;
+    console.log(this.value);
+});
--- main.ts
+++ main.ts
@@ -1,15 +1,95 @@
+class Student {
+    fullName: string;
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string,
 
-
-
-
-
-
-function greeter(person: String) {
-    return "Hello, " + person;
+    ) {
+        this.fullName = firstName + '' + middleInitial + '' + lastName;
+    }
 }
 
-let user = "Gerardo Garcia";
 
-document.body.textContent = greeter(user);
+
+
+
+
+interface Person {
+    firstName: string;
+    lastName: string;
+
+}
+
+
+
+
+
+
+const containerEl = document.getElementById('container')
+const btn = document.querySelector('.button')
+
+let number = 0
+
+
+const displayNumber = document.querySelector('.display-number')
+const btnReset = document.querySelector('.button-reset')
+
+btnReset.addEventListener('click', ()=>{
+     number = 0
+    let resetNumber = number.toString()
+
+     displayNumber.textContent = resetNumber
+})
+
+
+
+btn.addEventListener("click", ()=> {
+       console.log('i was clicked', number)
+
+
+
+
+
+
+
+       if(number >= 10){
+           number = 0
+       } else{
+            number++
+       }
+
+
+       let newNumber = number.toString()
+
+       displayNumber.textContent = newNumber
+
+
+   })
+
+
+const sliderCount = document.querySelector('.slider-number')
+const slider = document.querySelector('#slider')
+
+
+
+
+slider.addEventListener('input', function(){
+
+    let sliderValue = this.value
+
+    sliderCount.textContent = sliderValue;
+
+    console.log(this.value)
+
+
+})
+
+
+
+
+
+
+
 
 
